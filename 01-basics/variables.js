fcc8114daddba874865e6ee7f50d5c044a3cfb44
akhileshkrsingh <155524539+akhileshkrsingh@users.noreply.github.com>
2024-01-04 08:37:22 +0000

    scope of let const and var

--- 01-basics/variables.js
+++ 01-basics/variables.js
@@ -6,3 +6,25 @@ accountCity ="Prayagraj"
 accountId =2
 console.log(accountId) // it through error assignment to constant
 
+var a = 67 // a contains 67
+console.log(a)
+a = "akhilesh"
+console.log(a)
+// let 9akhil = 7 // Not allowed this will throw an error
+// let var = 7 
+
+console.log("JavaScript tutorial 3: var, let and const")
+// var a = 45;
+// var a = "p"
+let b = "akhile";
+const author = "akhil"
+author = 5 // Throws an error because constant cannot be changed
+b = 4
+const akhil = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
