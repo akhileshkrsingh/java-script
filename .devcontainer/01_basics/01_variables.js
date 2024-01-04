1e251b7af2dc3e8d82d1dbadc778ce8f1670a8c7
akhileshkrsingh <155524539+akhileshkrsingh@users.noreply.github.com>
2024-01-04 08:22:53 +0000

    delete file

--- .devcontainer/01_basics/01_variables.js
+++
@@ -1,8 +0,0 @@
-const accountId =1234
-let accountEmail = "akhilesh21221@gmail.com"
-var accountPassward ="dummyPass"
-
-accountCity ="Prayagraj"
-accountId =2
-console.log(accountId) // it through error assignment to constant
-
