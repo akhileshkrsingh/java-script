a657d48fa978e0918401fd35cd5ddf099a020e75
akhileshkrsingh <155524539+akhileshkrsingh@users.noreply.github.com>
2024-01-04 08:23:44 +0000

    add variable file

---
+++ 01-basics/variables.js
@@ -0,0 +1,8 @@
+const accountId =1234
+let accountEmail = "akhilesh21221@gmail.com"
+var accountPassward ="dummyPass"
+
+accountCity ="Prayagraj"
+accountId =2
+console.log(accountId) // it through error assignment to constant
+
